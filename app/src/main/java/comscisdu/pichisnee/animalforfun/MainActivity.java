fc5d94145d2cb80d5e0d96b5f90b6221a2868004
P18 <b.bigbanggame@gmail.com>
2020-10-20 15:31:30 +0700

    Link FB  Complate

--- app/src/main/java/comscisdu/pichisnee/animalforfun/MainActivity.java
+++ app/src/main/java/comscisdu/pichisnee/animalforfun/MainActivity.java
@@ -3,10 +3,12 @@ package comscisdu.pichisnee.animalforfun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,4 +43,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void clickFB(View view) {
+        Intent fbIntent = new Intent(Intent.ACTION_VIEW);
+        fbIntent.setData(Uri.parse("https://www.facebook.com/profile.php?id=100004519605938"));
+        startActivity(fbIntent);
+    }
+
+
 }
